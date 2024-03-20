bab7721a72110828fff190909e3b37cb6641ac89
Karolina Bargieł <karolinabargiel94@gmail.com>
2024-03-20 18:17:15 +0100

    first commit

---
+++ src/test/java/api/ExampleTest.java
@@ -0,0 +1,13 @@
+package api;
+
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class ExampleTest {
+
+    @Test
+    public void givenNonExistingPetIdWhenGetPetThenPetNotFoundTest() {
+        given().when().get("https://swaggerpetstore.przyklady.javastart.pl/v2/pet/0").then().statusCode(404);
+    }
+}
