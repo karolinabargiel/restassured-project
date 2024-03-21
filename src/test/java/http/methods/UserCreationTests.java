53191b21a9cd9740f692e82eeb419af1a5b9c5f2
Karolina Bargieł <karolinabargiel94@gmail.com>
2024-03-21 09:02:55 +0100

    create user test

---
+++ src/test/java/http/methods/UserCreationTests.java
@@ -0,0 +1,28 @@
+package http.methods;
+
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class UserCreationTests {
+
+    @Test
+    public void givenCorrectUserDataWhenCreateUserThenUserIsCreatedTest() {
+        String user = """
+                {
+                  "id": 445,
+                  "username": "firstuser",
+                  "firstName": "Krzysztof",
+                  "lastName": "Kowalski",
+                  "email": "krzysztof@test.com",
+                  "password": "password",
+                  "phone": "+123456789",
+                  "userStatus": 1
+                }
+                """;
+
+        given().log().all().body(user).contentType("application/json")
+                .when().post("https://swaggerpetstore.przyklady.javastart.pl/v2/user")
+                .then().log().all().statusCode(200);
+    }
+}
