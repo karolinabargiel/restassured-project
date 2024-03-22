47b062ebbe6ad1f60838041737882ea5c9c48695
Karolina Bargieł <karolinabargiel94@gmail.com>
2024-03-22 06:50:10 +0100

    add put and delete methods

--- src/test/java/http/methods/BasicHttpMethodsTests.java
+++ src/test/java/http/methods/BasicHttpMethodsTests.java
@@ -42,4 +42,113 @@ public class BasicHttpMethodsTests {
                 .when().get("https://swaggerpetstore.przyklady.javastart.pl/v2/pet/{petId}")
                 .then().log().all().statusCode(200);
     }
+
+    @Test
+    public void givenExistingPetWhenUpdatePetNameThenPetIsChangedTest() {
+
+        String pet = """
+                {
+                  "id": 69,
+                  "category": {
+                    "id": 1,
+                    "name": "cat"
+                  },
+                  "name": "Puszak",
+                  "photoUrls": [
+                    "http://photos.com/cat1.jpg"
+                  ],
+                  "tags": [
+                    {
+                      "id": 1,
+                      "name": "cat-category"
+                    }
+                  ],
+                  "status": "available"
+                }
+                """;
+
+        given().log().all().body(pet).contentType("application/json")
+                .when().post("https://swaggerpetstore.przyklady.javastart.pl/v2/pet")
+                .then().log().all().statusCode(200);
+
+        pet = """
+                {
+                  "id": 69,
+                  "category": {
+                    "id": 1,
+                    "name": "cat"
+                  },
+                  "name": "Kicia",
+                  "photoUrls": [
+                    "http://photos.com/cat1.jpg"
+                  ],
+                  "tags": [
+                    {
+                      "id": 1,
+                      "name": "cat-category"
+                    }
+                  ],
+                  "status": "available"
+                }
+                """;
+
+        given().log().all().body(pet).contentType("application/json")
+                .when().put("https://swaggerpetstore.przyklady.javastart.pl/v2/pet")
+                .then().log().all().statusCode(200);
+    }
+
+    @Test
+    public void givenExistingPetIdWhenDeletingPetThenIsDeletedTest() {
+
+        String pet = """
+                {
+                  "id": 888,
+                  "category": {
+                    "id": 1,
+                    "name": "cat"
+                  },
+                  "name": "Puszak",
+                  "photoUrls": [
+                    "http://photos.com/cat1.jpg"
+                  ],
+                  "tags": [
+                    {
+                      "id": 1,
+                      "name": "cat-category"
+                    }
+                  ],
+                  "status": "available"
+                }
+                """;
+
+        given().log().all().body(pet).contentType("application/json")
+                .when().post("https://swaggerpetstore.przyklady.javastart.pl/v2/pet")
+                .then().log().all().statusCode(200);
+
+        pet = """
+                {
+                  "id": 888,
+                  "category": {
+                    "id": 1,
+                    "name": "cat"
+                  },
+                  "name": "Puszak",
+                  "photoUrls": [
+                    "http://photos.com/cat1.jpg"
+                  ],
+                  "tags": [
+                    {
+                      "id": 1,
+                      "name": "cat-category"
+                    }
+                  ],
+                  "status": "available"
+                }
+                """;
+
+        given().log().all().body(pet).contentType("application/json")
+                .pathParam("petId", 888)
+                .when().delete("https://swaggerpetstore.przyklady.javastart.pl/v2/pet/{petId}")
+                .then().log().all().statusCode(200);
+    }
 }
