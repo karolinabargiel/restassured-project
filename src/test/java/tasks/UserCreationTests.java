8fda779c61455f23d9fd7bc36949d10011fcfe97
Karolina Bargieł <karolinabargiel94@gmail.com>
2024-03-25 18:41:09 +0100

    add deserialization class

--- src/test/java/tasks/UserCreationTests.java
+++ src/test/java/tasks/UserCreationTests.java
@@ -1,17 +1,19 @@
-package http.methods;
+package tasks;
 
 import org.testng.annotations.Test;
 import pojo.user.User;
 
 import static io.restassured.RestAssured.given;
 
-public class UserUpdateTests {
+public class UserCreationTests {
+
     User user = new User();
 
     @Test
-    public void givenCorrectUserDataWhenFirstNameLastNameAreUpdatedTest() {
-        user.setId(590);
-        user.setUsername("seconduser");
+    public void givenCorrectUserDataWhenCreateUserThenUserIsCreatedTest() {
+
+        user.setId(448);
+        user.setUsername("firstuser");
         user.setFirstName("Krzysztof");
         user.setLastName("Kowalski");
         user.setEmail("krzysztof@test.com");
@@ -21,15 +23,6 @@ public class UserUpdateTests {
 
         given().log().all().body(user).contentType("application/json")
                 .when().post("https://swaggerpetstore.przyklady.javastart.pl/v2/user")
-                .then().log().all().statusCode(200);
-
-        user.setFirstName("Damian");
-        user.setLastName("Gajos");
-
-        given().log().all().body(user)
-                .contentType("application/json")
-                .when().pathParam("username", user.getUsername())
-                .put("https://swaggerpetstore.przyklady.javastart.pl/v2/user/{username}")
                 .then().log().all().statusCode(200);
 
         given().log().all()
