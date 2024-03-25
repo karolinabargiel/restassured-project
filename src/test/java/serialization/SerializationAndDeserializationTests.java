02a2181e06ab03bf9b10c45b1e8409b728d18c1b
Karolina Bargieł <karolinabargiel94@gmail.com>
2024-03-25 19:09:10 +0100

    create new test

--- src/test/java/serialization/SerializationAndDeserializationTests.java
+++ src/test/java/serialization/SerializationAndDeserializationTests.java
@@ -42,4 +42,40 @@ public class SerializationAndDeserializationTests {
         assertEquals(actualPet.getTags().get(0).getName(), pet.getTags().get(0).getName(), "Pet tag name");
         assertEquals(actualPet.getStatus(), pet.getStatus(), "Pet status");
     }
+
+    @Test
+    public void givenExistingPetIdWhenGetPetThenReturnPetTest() {
+
+        Category category = new Category();
+        category.setId(1);
+        category.setName("dogs");
+
+        Tag tag = new Tag();
+        tag.setId(1);
+        tag.setName("dogs-category");
+
+        Pet pet = new Pet();
+        pet.setId(24);
+        pet.setCategory(category);
+        pet.setPhotoUrls(Collections.singletonList("http://photos.com/dog1.jpg"));
+        pet.setTags(Collections.singletonList(tag));
+        pet.setStatus("available");
+
+        Pet actualPet = given().log().all().body(pet).contentType("application/json")
+                .when().post("https://swaggerpetstore.przyklady.javastart.pl/v2/pet")
+                .then().log().all().statusCode(200).extract().as(Pet.class);
+
+        assertEquals(actualPet.getId(), pet.getId(), "Pet id");
+        assertEquals(actualPet.getCategory().getId(), pet.getCategory().getId(), "Category id");
+        assertEquals(actualPet.getCategory().getName(), pet.getCategory().getName(), "Category name");
+
+        Pet createdPet = given().log().method().log().uri()
+                .pathParam("petId", pet.getId())
+                .when().get("https://swaggerpetstore.przyklady.javastart.pl/v2/pet/{petId}")
+                .then().log().all().statusCode(200).extract().as(Pet.class);
+
+        assertEquals(actualPet.getId(), createdPet.getId(), "Pet id");
+        assertEquals(actualPet.getCategory().getId(), createdPet.getCategory().getId(), "Category id");
+        assertEquals(actualPet.getCategory().getName(), createdPet.getCategory().getName(), "Category name");
+    }
 }
